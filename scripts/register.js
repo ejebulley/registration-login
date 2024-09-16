7ced79c16a64ae10538879f08b5bba1149dd88a8
Justin Edie Ebulley <jediebulley@gmail.com>
2024-09-16 11:35:10 +0000

    registration login

---
+++ scripts/register.js
@@ -0,0 +1,13 @@
+// select form by name
+const registerForm = document.getElementById("register-form");
+// handle form submit event
+
+registerForm.onsubmit = function () {
+    
+    // collect username
+    // collect email
+    // collect password
+    // Save user information
+    // Send confirmation email
+
+}
